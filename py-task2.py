211c3a92a9ce669abb111fba37130bd9efed120d
Alexandr <jwchaganav@gmail.com>
2022-12-13 15:29:07 +0400

    проверил

---
+++ py-task2.py
@@ -0,0 +1,9 @@
+num = int(input("Введите трехзначное число: "))
+sum = 0
+rasr = 100
+if 99 < num < 1000:
+    while num > 0:
+        sum+=num//rasr
+        num = num%rasr
+        rasr = rasr//10
+print(sum)
